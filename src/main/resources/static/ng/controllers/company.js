7869bf4395c248538d354c6a452eae512c522027
rakesh <rak1994kumar@gmail.com>
2016-11-28 23:33:57 +0530

    company ctrl added

---
+++ src/main/resources/static/ng/controllers/company.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+'use strict';
+
+angular.module('walkingApp')
+  .controller('companyCtrl', function($scope,$location,$http) {
+	 
+	 console.log("Company Controller");	  
+  });
+
+
+
